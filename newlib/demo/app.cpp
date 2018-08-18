35f11ea5d36830919faa6417b80e1028b49523b2
Enrico Granata <granata.enrico@gmail.com>
2018-08-17 20:40:33 -0700

    newlib: implement enough logic to open / close a file

--- newlib/demo/app.cpp
+++ newlib/demo/app.cpp
@@ -7,5 +7,10 @@ int main() {
     printf("Write any integer to continue:\n");
     scanf("%i", &x);
     printf("\nYou wrote %d\n", x);
+    FILE* f = fopen("/devices/rtc/now", "r");
+    uint8_t *buffer = (uint8_t*)calloc(256, 1);
+    fread(buffer, 256, 1, f);
+    printf("into buffer %p I read %s\n", buffer, buffer);
+    fclose(f);
     exit(5);
 }
--- newlib/syscalls/interface.cpp
+++ newlib/syscalls/interface.cpp
@@ -10,13 +10,17 @@ extern "C" int  errno;
 #include <newlib/stdio.h>
 #include <newlib/syscalls.h>
 
+#include <kernel/syscalls/types.h>
+
 #define NEWLIB_IMPL_REQUIREMENT extern "C" 
 
 NEWLIB_IMPL_REQUIREMENT void _exit() {
     exit_syscall(12);
 }
 
-NEWLIB_IMPL_REQUIREMENT int close(int /*file*/) { return 0; }
+NEWLIB_IMPL_REQUIREMENT int close(int file) {
+    return fclose_syscall(file);
+}
 
 NEWLIB_IMPL_REQUIREMENT int execve(char *name, char **argv, char **env);
 
@@ -24,7 +28,9 @@ NEWLIB_IMPL_REQUIREMENT int fork();
 
 NEWLIB_IMPL_REQUIREMENT int fstat(int /*file*/, struct stat* /*st*/) { return 0; }
 
-NEWLIB_IMPL_REQUIREMENT int getpid();
+NEWLIB_IMPL_REQUIREMENT int getpid() {
+    return getpid_syscall() << 1;
+}
 
 NEWLIB_IMPL_REQUIREMENT int isatty(int file) {
     switch (file) {
@@ -39,11 +45,38 @@ NEWLIB_IMPL_REQUIREMENT int isatty(int file) {
 
 NEWLIB_IMPL_REQUIREMENT int kill(int pid, int sig);
 
-NEWLIB_IMPL_REQUIREMENT int link(char *oldname, char *newname);
+NEWLIB_IMPL_REQUIREMENT int link(char*, char*) {
+    errno = EMLINK;
+    return -1;
+}
 
 NEWLIB_IMPL_REQUIREMENT int lseek(int /*file*/, int /*ptr*/, int /*dir*/) { return 0; }
 
-NEWLIB_IMPL_REQUIREMENT int open(const char *name, int flags, ...);
+#define FLAG_TEST(flag) (flag == (flags & flag))
+
+NEWLIB_IMPL_REQUIREMENT int open(const char *name, int flags, ... /*mode: file permissions not supported - ignore*/) {
+    uint32_t puppy_flags = 0;
+    if (FLAG_TEST(O_RDONLY)) {
+        puppy_flags = FILE_OPEN_READ;
+    } else if (FLAG_TEST(O_WRONLY)) {
+        puppy_flags = FILE_OPEN_WRITE;
+    } else if (FLAG_TEST(O_RDWR)) {
+        puppy_flags = FILE_OPEN_READ | FILE_OPEN_WRITE;
+    }
+
+    if (FLAG_TEST(O_APPEND)) {
+        puppy_flags |= FILE_OPEN_APPEND;
+    }
+    if (FLAG_TEST(O_TRUNC)) {
+        puppy_flags |= FILE_OPEN_NEW;
+    }
+
+    auto fd = fopen_syscall(name, puppy_flags);
+    if (fd & 1) return -1;
+    return fd >> 1;
+}
+
+#undef FLAG_TEST
 
 NEWLIB_IMPL_REQUIREMENT int read(int file, char* ptr, int len) {
     // special case non-blocking stdin
