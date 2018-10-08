73cda6674836c2448f41a3c71a3735f3616e00f8
Enrico Granata <granata.enrico@gmail.com>
2018-10-07 18:24:26 -0700

    msgqueue: add test cases to validate msgqueue implementation

--- tests/newmessage/test.cpp
+++ tests/newmessage/test.cpp
@@ -65,10 +65,138 @@ class ReceiveMessageTest : public Test {
         }
 };
 
+static void MessageOrderTest_sender() {
+    FILE* f = fopen("/queues/newmessage/MessageOrder", "w");
+    if (f == nullptr) exit(1);
+    setvbuf(f, nullptr, _IOFBF, 4096);
+    uint32_t msg = 0xBeefF00d;
+    fwrite(&msg, sizeof(msg), 1, f);
+    fflush(f);
+    msg = 0xA11DF00d;
+    fwrite(&msg, sizeof(msg), 1, f);
+    fflush(f);
+    exit(0);
+}
+class MessageOrderTest : public Test {
+    public:
+        MessageOrderTest() : Test("newmessage.MessageOrder") {}
+    
+    protected:
+        void run() override {
+            FILE* f = fopen("/queues/newmessage/MessageOrder", "r");
+            CHECK_NOT_EQ(f, nullptr);
+            setvbuf(f, nullptr, _IOFBF, 4096);
+            kpid_t spid = clone(MessageOrderTest_sender);
+            CHECK_NOT_EQ(spid, 0);
+            auto sexit = collect(spid);
+            CHECK_EQ(sexit.reason, process_exit_status_t::reason_t::cleanExit);
+            CHECK_EQ(sexit.status, 0);
+
+            new_message_t msg1;
+            CHECK_EQ(sizeof(msg1), fread(&msg1, 1, sizeof(msg1), f));
+            CHECK_EQ(msg1.header.sender, spid);
+            CHECK_EQ(msg1.header.payload_size, sizeof(pid_t));
+            uint32_t *val = (uint32_t*)&msg1.payload[0];
+            CHECK_NOT_EQ(val, nullptr);
+            CHECK_EQ(*val, 0xBeefF00d);
+
+            new_message_t msg2;
+            CHECK_EQ(sizeof(msg2), fread(&msg2, 1, sizeof(msg2), f));
+            CHECK_EQ(msg2.header.sender, spid);
+            CHECK_EQ(msg2.header.payload_size, sizeof(pid_t));
+            val = (uint32_t*)&msg2.payload[0];
+            CHECK_NOT_EQ(val, nullptr);
+            CHECK_EQ(*val, 0xA11DF00d);
+
+            fclose(f);
+        }
+};
+
+static void TwoSendersTest_sender1() {
+    FILE* f = fopen("/queues/newmessage/TwoSenders", "w");
+    if (f == nullptr) exit(1);
+    setvbuf(f, nullptr, _IOFBF, 4096);
+    uint32_t msg = 0xAA55BB44;
+    sleep(3);
+    fwrite(&msg, sizeof(msg), 1, f);
+    fflush(f);
+    sleep(1);
+    exit(0);
+}
+static void TwoSendersTest_sender2() {
+    FILE* f = fopen("/queues/newmessage/TwoSenders", "w");
+    if (f == nullptr) exit(1);
+    setvbuf(f, nullptr, _IOFBF, 4096);
+    uint32_t msg = 0xCC00AA88;
+    sleep(1);
+    fwrite(&msg, sizeof(msg), 1, f);
+    fflush(f);
+    sleep(1);
+    exit(0);
+}
+class TwoSendersTest : public Test {
+    public:
+        TwoSendersTest() : Test("newmessage.TwoSenders") {}
+    
+    protected:
+        void run() override {
+            FILE* f = fopen("/queues/newmessage/TwoSenders", "r");
+            CHECK_NOT_EQ(f, nullptr);
+            setvbuf(f, nullptr, _IOFBF, 4096);
+            kpid_t spid1 = clone(TwoSendersTest_sender1);
+            CHECK_NOT_EQ(spid1, 0);
+            kpid_t spid2 = clone(TwoSendersTest_sender2);
+            CHECK_NOT_EQ(spid2, 0);
+            auto sexit1 = collect(spid1);
+            CHECK_EQ(sexit1.reason, process_exit_status_t::reason_t::cleanExit);
+            CHECK_EQ(sexit1.status, 0);
+            auto sexit2 = collect(spid2);
+            CHECK_EQ(sexit2.reason, process_exit_status_t::reason_t::cleanExit);
+            CHECK_EQ(sexit2.status, 0);
+
+            new_message_t msg1;
+            CHECK_EQ(sizeof(msg1), fread(&msg1, 1, sizeof(msg1), f));
+            new_message_t msg2;
+            CHECK_EQ(sizeof(msg2), fread(&msg2, 1, sizeof(msg2), f));
+
+            CHECK_EQ(sizeof(uint32_t), msg1.header.payload_size);
+            CHECK_EQ(sizeof(uint32_t), msg2.header.payload_size);
+
+            uint32_t *msg1_payload = (uint32_t*)&msg1.payload[0];
+            uint32_t *msg2_payload = (uint32_t*)&msg2.payload[0];
+            CHECK_NOT_EQ(msg1_payload, nullptr);
+            CHECK_NOT_EQ(msg2_payload, nullptr);
+
+            if (msg1.header.sender == spid1) {
+                CHECK_EQ(*msg1_payload, 0xAA55BB44);
+                CHECK_EQ(msg2.header.sender, spid2);
+            } else if (msg1.header.sender == spid2) {
+                CHECK_EQ(*msg1_payload, 0xCC00AA88);
+                CHECK_EQ(msg2.header.sender, spid1);
+            } else {
+                CHECK_TRUE(false);
+            }
+
+            if (msg2.header.sender == spid1) {
+                CHECK_EQ(*msg2_payload, 0xAA55BB44);
+                CHECK_EQ(msg1.header.sender, spid2);
+            } else if (msg2.header.sender == spid2) {
+                CHECK_EQ(*msg2_payload, 0xCC00AA88);
+                CHECK_EQ(msg1.header.sender, spid1);
+            } else {
+                CHECK_TRUE(false);
+            }
+
+            fclose(f);
+        }
+};
+
 int main() {
     auto& testPlan = TestPlan::defaultPlan(TEST_NAME);
 
-    testPlan.add<ReceiveMessageTest>();
+    testPlan.add<ReceiveMessageTest>()
+            .add<MessageOrderTest>()
+            .add<TwoSendersTest>();
 
     testPlan.test();
     return 0;
