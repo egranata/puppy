a0d26858a0901fe6a3449bc319114f2af29e05b5
Enrico Granata <granata.enrico@gmail.com>
2018-10-21 22:06:35 +0000

    newlib: fix the include guard for klog.h

--- newlib/include/impl/klog.h
+++ newlib/include/impl/klog.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef NEWLIB_ABSOLUTIZE
-#define NEWLIB_ABSOLUTIZE
+#ifndef NEWLIB_KLOG
+#define NEWLIB_KLOG
 
 #include <newlib/impl/scoped_ptr.h>
 
