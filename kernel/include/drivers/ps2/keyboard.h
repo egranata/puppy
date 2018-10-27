d731ac2cd652856e4e44e0d9f36de22cef944024
Enrico Granata <granata.enrico@gmail.com>
2018-10-26 21:00:39 -0700

    keyboard: introduce keycodes for arrow and function keys
    These are the first HBS keycodes
    
    Their values are such that currently pressing the keys causes the TTY to generate letter values
    This is convenient in order to detect mishandling at the higher software layers

--- kernel/include/drivers/ps2/keyboard.h
+++ kernel/include/drivers/ps2/keyboard.h
@@ -23,6 +23,24 @@
 class PS2Keyboard : public PS2Controller::Device {
     public:
         struct key_event_t {
+            static constexpr uint16_t UP    = 0x0165;
+            static constexpr uint16_t LFT   = 0x0166;
+            static constexpr uint16_t RHT   = 0x0167;
+            static constexpr uint16_t DWN   = 0x0168;
+
+            static constexpr uint16_t F1         = 0x0265;
+            static constexpr uint16_t F2         = 0x0266;
+            static constexpr uint16_t F3         = 0x0267;
+            static constexpr uint16_t F4         = 0x0268;
+            static constexpr uint16_t F5         = 0x0269;
+            static constexpr uint16_t F6         = 0x0270;
+            static constexpr uint16_t F7         = 0x0271;
+            static constexpr uint16_t F8         = 0x0272;
+            static constexpr uint16_t F9         = 0x0273;
+            static constexpr uint16_t F10        = 0x0274;
+            static constexpr uint16_t F11        = 0x0275;
+            static constexpr uint16_t F12        = 0x0276;
+
             enum class keymap_to_use : uint8_t {
                 CAPS_LOCK,
                 SHIFT,
