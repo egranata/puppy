f412047265fa949d29b57f2315ebc943ed86e18a
Enrico Granata <granata.enrico@gmail.com>
2018-10-27 21:45:35 +0000

    qemu: force keyboard layout
    This solves an issue (see e.g. https://askubuntu.com/questions/1007720/qemu-keys-not-functional)
    in which arrow keys may end up not reported correctly to the emulated OS if qemu is run inside
    a container

--- build/qemu.sh
+++ build/qemu.sh
@@ -3,4 +3,5 @@
 qemu-system-i386 -drive format=raw,media=disk,file=out/os.img -vga std \
 -serial file:out/kernel.log -m 768 -d guest_errors -rtc base=localtime \
 -monitor stdio \
--smbios type=0,vendor="Puppy" -smbios type=1,manufacturer="Puppy",product="Puppy System",serial="P0PP1"
+-smbios type=0,vendor="Puppy" -smbios type=1,manufacturer="Puppy",product="Puppy System",serial="P0PP1" \
+-k en-us
