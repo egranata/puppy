517ac02ab14fc82d173e03b386b94d63370475c7
Enrico Granata <granata.enrico@gmail.com>
2018-10-26 20:24:18 -0700

    keyboard: do not emit fake ASCII codes for modifier keys

--- kernel/include/drivers/ps2/keyboard.h
+++ kernel/include/drivers/ps2/keyboard.h
@@ -29,14 +29,6 @@ class PS2Keyboard : public PS2Controller::Device {
                 LOWERCASE,
                 LONG
             };
-            static constexpr uint8_t LEFT_SHIFT = 1;
-            static constexpr uint8_t RIGHT_SHIFT = 2;
-            static constexpr uint8_t LEFT_CTRL = 3;
-            static constexpr uint8_t RIGHT_CTRL = 4;
-            static constexpr uint8_t LEFT_ALT = 5;
-            static constexpr uint8_t RIGHT_ALT = 6;
-            static constexpr uint8_t DELETE = 7;
-            static constexpr uint8_t BACKSPACE = 8;
 
             keymap_to_use keymap;
             bool ctrldown;
