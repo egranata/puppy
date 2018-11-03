ad14a6a2d7dd13244932685fb5fd20582ac11cf6
Enrico Granata <granata.enrico@gmail.com>
2018-11-02 19:26:24 -0700

    micropython: basic file I/O
    Allows opening a file and reading/writing from it
    
    https://github.com/egranata/puppy/issues/91

--- third_party/micropython/ports/puppy/main.c
+++ third_party/micropython/ports/puppy/main.c
@@ -15,6 +15,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "py/compile.h"
 #include "py/runtime.h"
@@ -40,11 +41,6 @@ void gc_collect(void) { }
 mp_import_stat_t mp_import_stat(const char *path) {
     return MP_IMPORT_STAT_NO_EXIST;
 }
-
-mp_obj_t mp_builtin_open(size_t n_args, const mp_obj_t *args, mp_map_t *kwargs) {
-    return mp_const_none;
-}
-MP_DEFINE_CONST_FUN_OBJ_KW(mp_builtin_open_obj, 1, mp_builtin_open);
 
 static mp_obj_t pystack[1024];
 
