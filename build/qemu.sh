09dd012fb3b2ff8438f147b41edc1e232f04e9ce
Enrico Granata <granata.enrico@gmail.com>
2018-11-11 10:28:15 -0800

    [skip travis] pick a CPU model for QEMU to simulate
    The N270 is a "netbook-style" Atom: https://ark.intel.com/products/36331/Intel-Atom-Processor-N270-512K-Cache-1-60-GHz-533-MHz-FSB-

--- build/qemu.sh
+++ build/qemu.sh
@@ -4,4 +4,4 @@ qemu-system-i386 -drive format=raw,media=disk,file=out/os.img -vga std \
 -serial file:out/kernel.log -m 768 -d guest_errors -rtc base=utc \
 -monitor stdio \
 -smbios type=0,vendor="Puppy" -smbios type=1,manufacturer="Puppy",product="Puppy System",serial="P0PP1" \
--k en-us
+-k en-us -cpu n270
--- build/test.py
+++ build/test.py
@@ -18,7 +18,7 @@ import json, os, subprocess, time, sys, tempfile
 
 CMDLINE='qemu-system-i386 -drive format=raw,media=disk,file=out/os.img -display none -serial file:out/kernel.log -m 768 -d guest_errors ' + \
         '-rtc base=utc -monitor stdio -smbios type=0,vendor="Puppy" -smbios type=1,manufacturer="Puppy",product="Puppy System",serial="P0PP1" ' + \
-        '-k en-us'
+        '-k en-us -cpu n270'
 
 def ensureGone(path):
     try:
